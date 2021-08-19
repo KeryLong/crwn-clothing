bb70ebbed97b84e4626dfcd957cf18bf0335888a
KeryLong <kery.long@gmail.com>
2021-08-19 15:34:49 -0600

    integrated firebase auth and storing users in our own database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,33 @@ messagingSenderId: "480421411842",
 appId: "1:480421411842:web:35de17c8a21de967233b1b",
 measurementId: "G-Z5JLH01GY0"
 };
+
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
